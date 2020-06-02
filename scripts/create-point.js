d59675ffd78902859563e6480d51bbdec405d021
maykbrito <maykbrito@gmail.com>
2020-06-02 15:16:15 -0300

    day 3 part 3: selected items logic ok

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -48,3 +48,56 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+// Itens de coleta
+// pegar todos os li`s
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+
+
+
+    // verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    // se já estiver selecionado, 
+    if( alreadySelected >= 0 ) {
+        // tirar da selecao
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+}
