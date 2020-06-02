d5db7199197de1a848e3400a94af5aa5b61e94b3
maykbrito <maykbrito@gmail.com>
2020-06-01 23:02:44 -0300

    day 2 part7

---
+++ scripts/create-point.js
@@ -0,0 +1,46 @@
+
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+    })
+}
+
+populateUFs()
+
+
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
