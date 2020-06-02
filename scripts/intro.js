31bdf4ed7bd624c82fac96324989b6e354b2159e
maykbrito <maykbrito@gmail.com>
2020-06-01 22:21:40 -0300

    day 2 part6

---
+++ scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é um string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalides - Atalhos - Reuso de código
+
+// // registrar função
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
