c9556f873869e8e349ef73fdb8967a4362a31330
maykbrito <maykbrito@gmail.com>
2020-06-03 17:33:04 -0300

    day 4 part 2: template engine

--- src/server.js
+++ src/server.js
@@ -2,12 +2,32 @@ const express = require("express")
 const server = express()
 
 
+// configurar pasta publica
+server.use(express.static("public"))
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+    return res.render("index.html", { title: "Um título"})
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
