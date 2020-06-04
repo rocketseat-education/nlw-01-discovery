3e58f6d4a017eed49d4881d39d5678913efc87a0
maykbrito <maykbrito@gmail.com>
2020-06-04 17:54:42 -0300

    dia 5 part 3

--- src/server.js
+++ src/server.js
@@ -84,13 +84,17 @@ server.post("/savepoint", (req, res) => {
 
 
 
-
-
-
 server.get("/search", (req, res) => {
 
+    const search = req.query.search
+
+    if(search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
     // pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
